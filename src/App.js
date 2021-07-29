5be676678759df7faed4ef6623c9aaa9441017ed
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-07-29 11:57:57 +0200

    install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './Containers/Home.jsx';
 import './App.css';
+import './App.scss';
 import Header from './Components/Header/Header.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 require('dotenv').config()
